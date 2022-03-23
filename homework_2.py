d49d1c56ae3b840e59a9bb00af9ebde53455697a
Adicj <petre82adrian@gmail.com>
2022-03-23 22:32:08 +0200

    SCR: 1st parameter removed (I read between lines first time)

--- homework_2.py
+++ homework_2.py
@@ -1,21 +1,22 @@
 print('This is my 2\'nd homework!')
 
 
-def my_complete_sum(a, b):
+def my_complete_sum( b):
     a = 0
-    return sum(x for x in range(a, b))
+    return sum(x for x in range(a, b+1))
 
 
-def my_even_sum(a, b):
+def my_even_sum(b):
     a = 0
-    return sum(x for x in range(a, b) if x % 2 == 0)
+    return sum(x for x in range(a, b+1) if x % 2 == 0)
 
 
-def my_odd_sum(a, b):
+def my_odd_sum(b):
     a = 0
-    return sum(x for x in range(a, b) if x % 2 == 1)
+    return sum(x for x in range(a, b+1) if x % 2 == 1)
 
 
-print('My sum for all the numbers inside a range is: ', my_complete_sum(99, 11))
-print('My sum for even numbers inside a range is: ', my_even_sum(1, 5))
-print('My sum for even numbers inside a range is: ', my_odd_sum(1000000, 10))
+print(f'My sum for all the numbers inside a range is: ', my_complete_sum(3))
+print('My sum for even numbers inside a range is: ', my_even_sum(9))
+print('My sum for even numbers inside a range is: ', my_odd_sum(10))
+
