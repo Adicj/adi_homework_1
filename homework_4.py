305fde3f09453f46f3bdc5b7c24080e925b8a8c4
Adicj <petre82adrian@gmail.com>
2022-04-19 15:55:35 +0300

    SCR: Spelling corrected.

--- homework_4.py
+++ homework_4.py
@@ -1,54 +1,41 @@
-if __name__ == '__homework_4__':
-    pass
+def reduce_fraction(m, n):
+    while m % n != 0:
+        old_m = m
+        old_n = n
 
-
-def reduce(m,n):
-    while m%n != 0:
-        oldm = m
-        oldn = n
-
-        m = oldn
-        n = oldm%oldn
+        m = old_n
+        n = old_m % old_n
     return n
 
 
 class Fraction:
     def __init__(self, n, d):
-        self.num = int(n / reduce(abs(n), abs(d)))
-        self.denom = int(d / reduce(abs(n), abs(d)))
-        if self.denom < 0:
-            self.denom = abs(self.denom)
+        self.num = int(n / reduce_fraction(abs(n), abs(d)))
+        self.den = int(d / reduce_fraction(abs(n), abs(d)))
+        if self.den < 0:
+            self.den = abs(self.den)
             self.num = -1*self.num
-        elif self.denom == 0:
+        elif self.den == 0:
             raise ZeroDivisionError
 
-    def __add__(self, otherfraction):
+    def __add__(self, other_fraction):
+        new_num = self.num * other_fraction.den + self.den * other_fraction.num
+        new_den = self.den * other_fraction.den
+        return Fraction(new_num, new_den)
 
-        newnum = self.num * otherfraction.denom + self.denom * otherfraction.num
-        newden = self.denom * otherfraction.denom
-        return Fraction(newnum, newden)
-
-    def __sub__(self, otherfraction):
-        newnum = self.num * otherfraction.denom - self.denom * otherfraction.num
-        newden = self.denom * otherfraction.denom
-        return Fraction(newnum, newden)
+    def __sub__(self, other_fraction):
+        new_num = self.num * other_fraction.den - self.den * other_fraction.num
+        new_den = self.den * other_fraction.den
+        return Fraction(new_num, new_den)
 
     def __str__(self):
-        if type(self) is tuple:
-            if self[1] < 0:
-                return '%s/%s' % (self[0], -1 * self[1])
-            else:
-                return '%s/%s' % (self[0], self[1])
-        elif self.denom == 1:
-            return str(self.num)
-        else:
-            return '%s/%s' % (self.num, self.denom)
+        return str(self.num) + "/" + str(self.den)
 
     def invert(self):
-        return Fraction(self.denom, self.num)
+        return Fraction(self.den, self.num)
 
 
-x1 = Fraction(4, 3)
+x1 = Fraction(5, 18)
 x2 = Fraction(2, 3)
 x3 = x1 + x2
 x4 = x1 - x2
@@ -56,5 +43,5 @@ x5 = Fraction.invert(x1)
 
 print('First fraction is: ', x1, 'Second fraction is: ', x2)
 print('Sum of fraction is: ', x3)
-print('Substract of fraction is: ', x4)
+print('Subtract of fraction is: ', x4)
 print('Inverted first fraction is: ', x5)
